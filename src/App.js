c94fd20e98bb520c4659a1990276da4178545006
Adam Osburn <wizardofosburn@gmail.com>
2020-05-09 16:06:38 -0400

    set up state and passed it to Form.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [members, setMembers] = useState({
+    name: '',
+    email: '',
+    role: '',
+  })
+
   return (
-    
+    <Form members={members}/>
     );
 }
 
